1ba875d15a5bfbf04e97d8bf79419af599f71142
thorhsu <thorshu@gmail.com>
2019-01-21 16:10:50 +0800

    Updates

---
+++ precache-manifest.293cf29c6afe1131cf0e49f631daddb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65de880bcb8ec8577345",
+    "url": "/static/js/main.65de880b.chunk.js"
+  },
+  {
+    "revision": "a649b6d655269530a6ac",
+    "url": "/static/js/1.a649b6d6.chunk.js"
+  },
+  {
+    "revision": "65de880bcb8ec8577345",
+    "url": "/static/css/main.a5417abb.chunk.css"
+  },
+  {
+    "revision": "1833469b02533f119a17dd1f3957684b",
+    "url": "/index.html"
+  }
+];
